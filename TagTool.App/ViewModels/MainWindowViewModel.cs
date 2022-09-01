27dbf64dd2601ef30313fc3a4d6ab344d4e2eff0
tczyz <tomczyz97@gmail.com>
2022-09-01 09:57:53 +0200

    initial commit

---
+++ TagTool.App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace TagTool.App.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ TagTool.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace TagTool.App.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
