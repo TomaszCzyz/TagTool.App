e047f786bbeca977ff146c2de1e170393b93e2e4
tczyz <tomczyz97@gmail.com>
2022-10-28 21:51:45 +0200

    improve IconToBitmapConverter.cs

--- src/TagTool.App/Converters/IconToBitmapConverter.cs
+++ src/TagTool.App/Converters/IconToBitmapConverter.cs
@@ -1,35 +1,42 @@
 ﻿using System.Drawing.Imaging;
 using System.Globalization;
+using Avalonia;
 using Avalonia.Data.Converters;
 using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using Microsoft.Extensions.DependencyInjection;
 using TagTool.App.Core.Services;
 
 namespace TagTool.App.Converters;
 
 public class IconToBitmapConverter : IValueConverter
 {
-    private readonly DefaultFileIconProvider _defaultFileIconProvider = new();
+    private readonly IFileIconProvider _defaultFileIconProvider = App.Current.Services.GetRequiredService<IFileIconProvider>();
+    private static readonly IAssetLoader _assets = AvaloniaLocator.Current.GetRequiredService<IAssetLoader>();
 
-    private static readonly Bitmap _defaultFileIcon
-        = new(@"C:\Users\tczyz\Source\Repos\My\TagTool\TagTool.App\TagTool.App\Assets/Images/round_description_black_36dp.png");
-
-    private static readonly Bitmap _defaultFolderIcon
-        = new(@"C:\Users\tczyz\Source\Repos\My\TagTool\TagTool.App\TagTool.App\Assets/Images/windows_folder_icon.png");
+    private static readonly Stream _defaultFileIcon = _assets.Open(new Uri("avares://TagTool.App/Assets/Images/round_description_black_36dp.png"));
+    private static readonly Stream _defaultFolderIcon = _assets.Open(new Uri("avares://TagTool.App/Assets/Images/windows_folder_icon.png"));
 
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value is not string path) return null;
 
+        int length = default;
+        _ = parameter is string s && int.TryParse(s, out length);
+
         if (Directory.Exists(path))
         {
-            return _defaultFolderIcon;
+            return CreateBitmap(_defaultFolderIcon, length);
         }
 
 #pragma warning disable CA1416
+        // todo: make this behaviour platform agnostic
+
         var bitmap = _defaultFileIconProvider.GetFileIcon(path)?.ToBitmap();
+
         if (bitmap is null)
         {
-            return _defaultFileIcon;
+            return CreateBitmap(_defaultFileIcon, length);
         }
 
         using var memoryStream = new MemoryStream();
@@ -37,14 +44,25 @@ public class IconToBitmapConverter : IValueConverter
         memoryStream.Flush();
         memoryStream.Seek(0, SeekOrigin.Begin);
 
-        return parameter is string s && int.TryParse(s, out var length)
-            ? Bitmap.DecodeToWidth(memoryStream, length)
-            : new Bitmap(memoryStream);
+        return CreateBitmap(memoryStream, length);
 #pragma warning restore CA1416
     }
 
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         throw new NotImplementedException();
+    }
+
+    private static Bitmap CreateBitmap(Stream stream, int length)
+    {
+        if (length == default)
+        {
+            return new Bitmap(stream);
+        }
+
+        var decodeToWidth = Bitmap.DecodeToWidth(stream, length);
+        stream.Seek(0, SeekOrigin.Begin);
+
+        return decodeToWidth;
     }
 }
