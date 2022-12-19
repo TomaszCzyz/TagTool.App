135cd67cf5d924df8f6e1607ffe3fd80052f6b3a
tczyz <tomczyz97@gmail.com>
2022-12-19 23:39:25 +0100

    extend app to client area and add dragging

--- src/TagTool.App/Views/MainWindow.axaml.cs
+++ src/TagTool.App/Views/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using Microsoft.Extensions.DependencyInjection;
 using TagTool.App.ViewModels;
@@ -14,12 +15,38 @@ public partial class MainWindow : Window
         InitializeComponent();
 #if DEBUG
         this.AttachDevTools();
-        Renderer.DrawFps = false;
+        Renderer.DrawFps = true;
 #endif
     }
 
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+    }
+
+    private bool _mouseDownForWindowMoving;
+    private PointerPoint _originalPoint;
+
+    private void InputElement_OnPointerMoved(object? sender, PointerEventArgs e)
+    {
+        if (!_mouseDownForWindowMoving) return;
+
+        var currentPoint = e.GetCurrentPoint(this);
+        Position = new PixelPoint(
+            Position.X + (int)(currentPoint.Position.X - _originalPoint.Position.X),
+            Position.Y + (int)(currentPoint.Position.Y - _originalPoint.Position.Y));
+    }
+
+    private void InputElement_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (WindowState is WindowState.Maximized or WindowState.FullScreen) return;
+
+        _mouseDownForWindowMoving = true;
+        _originalPoint = e.GetCurrentPoint(this);
+    }
+
+    private void InputElement_OnPointerReleased(object? sender, PointerReleasedEventArgs e)
+    {
+        _mouseDownForWindowMoving = false;
     }
 }
