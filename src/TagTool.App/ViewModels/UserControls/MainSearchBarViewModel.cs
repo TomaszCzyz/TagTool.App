60ffb81b94351722c91473e703e43cc60a116e13
tczyz <tomczyz97@gmail.com>
2022-12-21 15:20:31 +0100

    add more functionalities to MainSearchBar

---
+++ src/TagTool.App/ViewModels/UserControls/MainSearchBarViewModel.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DynamicData;
+using JetBrains.Annotations;
+using TagTool.App.Core.Models;
+
+namespace TagTool.App.ViewModels.UserControls;
+
+public partial class MainSearchBarViewModel : ViewModelBase, ITagsContainer
+{
+    [ObservableProperty]
+    private string? _searchText;
+
+    [ObservableProperty]
+    private Tag? _selectedItemFromSearched;
+
+    [ObservableProperty]
+    private Tag? _selectedItemFromPopular;
+
+    public ObservableCollection<Tag> SearchResults { get; set; } = new();
+
+    public ObservableCollection<Tag> PopularTags { get; set; } = new();
+
+    public ObservableCollection<object> EnteredTags { get; set; } = new();
+
+    public Tag[] Tags => EnteredTags.Where(o => o.GetType() == typeof(Tag)).Cast<Tag>().ToArray();
+
+    /// <summary>
+    ///     ctor for XAML previewer
+    /// </summary>
+    public MainSearchBarViewModel()
+    {
+    }
+
+    [UsedImplicitly]
+    public MainSearchBarViewModel(IServiceProvider serviceProvider)
+    {
+        var tags = new Tag[] { new("Audio"), new("Dog"), new("Picture"), new("Colleague"), new("Tag6"), new("LastTag") };
+        EnteredTags.AddRange(tags);
+
+        var popularTags = new Tag[] { new("SomeTag"), new("Tag"), new("SomeTag"), new("Picture"), new("Tag"), new("Picture"), new("Picture") };
+        PopularTags.AddRange(popularTags);
+
+        var searchResults = new Tag[] { new("Result1"), new("Tag"), new("Result2"), new("Picture"), new("SearchTag") };
+        SearchResults.AddRange(searchResults);
+
+        EnteredTags.Add("");
+    }
+
+    [RelayCommand]
+    private void RemoveTag(Tag tag)
+    {
+        Remove(tag);
+    }
+
+    [RelayCommand]
+    private void UpdateSearch()
+    {
+        SelectedItemFromSearched = SearchResults.FirstOrDefault();
+    }
+
+    [RelayCommand]
+    private void AddTag()
+    {
+        var itemToAdd = SelectedItemFromSearched ?? SelectedItemFromPopular;
+
+        if (itemToAdd is null || EnteredTags.Contains(itemToAdd)) return;
+
+        SearchText = "";
+        SearchResults.Remove(itemToAdd);
+
+        AddTag(itemToAdd);
+    }
+
+    public void AddTag(Tag tag)
+    {
+        EnteredTags.Insert(EnteredTags.Count - 1, tag);
+    }
+
+    public void Remove(Tag tag)
+    {
+        EnteredTags.Remove(tag);
+    }
+
+    public void RemoveLast()
+    {
+        var lastTag = EnteredTags.LastOrDefault(o => o.GetType() == typeof(Tag));
+
+        if (lastTag is null) return;
+
+        EnteredTags.Remove(lastTag);
+    }
+}
--- src/TagTool.App/Views/UserControls/MainSearchBar.axaml.cs
+++ src/TagTool.App/Views/UserControls/MainSearchBar.axaml.cs
@@ -1,7 +1,7 @@
-﻿using Avalonia;
-using Avalonia.Controls;
+﻿using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.LogicalTree;
 using Avalonia.Markup.Xaml;
 using Microsoft.Extensions.DependencyInjection;
 using TagTool.App.ViewModels.UserControls;
@@ -12,7 +12,7 @@ public partial class MainSearchBar : UserControl
 {
     public MainSearchBar()
     {
-        DataContext = App.Current.Services.GetRequiredService<SimpleTagsBarViewModel>();
+        DataContext = App.Current.Services.GetRequiredService<MainSearchBarViewModel>();
         InitializeComponent();
     }
 
@@ -21,31 +21,54 @@ public partial class MainSearchBar : UserControl
         AvaloniaXamlLoader.Load(this);
     }
 
-    private void TagSearchAutoCompleteBox_OnAttachedToVisualTree(object? sender, VisualTreeAttachmentEventArgs e)
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
     {
-        if (sender is not AutoCompleteBox { DataContext: TagSearchBoxViewModel } autoCompleteBox) return;
+        SearchHelperPopup.IsOpen ^= true;
+    }
 
-        autoCompleteBox.AddHandler(KeyDownEvent, OnKeyDown, RoutingStrategies.Tunnel);
+    private void InputElement_OnGotFocus(object? sender, GotFocusEventArgs e)
+    {
+        SearchHelperPopup.IsOpen = true;
+    }
+
+    private void StyledElement_OnAttachedToLogicalTree(object? sender, LogicalTreeAttachmentEventArgs e)
+    {
+        var textBox = (TextBox)sender!;
+
+        textBox.AddHandler(KeyDownEvent, OnKeyDown, RoutingStrategies.Tunnel);
+        textBox.AddHandler(PointerPressedEvent, PointerPressed, RoutingStrategies.Tunnel);
+    }
+
+    private new void PointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        SearchHelperPopup.IsOpen = true;
     }
 
     private void OnKeyDown(object? sender, KeyEventArgs e)
     {
-        if (sender is not AutoCompleteBox { DataContext: TagSearchBoxViewModel viewModel } autoCompleteBox) return;
+        var textBox = (TextBox)sender!;
+        var viewModel = (MainSearchBarViewModel)DataContext!;
 
         switch (e.Key)
         {
-            case Key.Enter when autoCompleteBox.SelectedItem is not null:
-                viewModel.CommitTagCommand.Execute(e);
+            case Key.Enter: // when autoCompleteBox.SelectedItem is not null:
+                viewModel.AddTagCommand.Execute(e);
                 e.Handled = true;
                 break;
-            case Key.Back when string.IsNullOrEmpty(autoCompleteBox.Text):
+            case Key.Back when string.IsNullOrEmpty(textBox.Text):
                 viewModel.RemoveTagCommand.Execute(e);
                 break;
+            case Key.Right:
+                SearchResultsListBox.SelectedIndex++;
+                e.Handled = true;
+                break;
+            case Key.Left:
+                SearchResultsListBox.SelectedIndex--;
+                e.Handled = true;
+                break;
+            default:
+                viewModel.UpdateSearchCommand.Execute(e);
+                break;
         }
-    }
-
-    private void Button_OnClick(object? sender, RoutedEventArgs e)
-    {
-        SearchHelperPopup.IsOpen = true;
     }
 }
