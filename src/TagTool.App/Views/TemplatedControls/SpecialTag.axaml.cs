1a98355708ffbd8dac5af98d8ce30a0bf4c25552
tczyz <tomczyz97@gmail.com>
2023-01-14 01:29:12 +0100

    add prototype of control template for special tag

---
+++ src/TagTool.App/Views/TemplatedControls/SpecialTag.axaml.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+
+namespace TagTool.App.Views.TemplatedControls;
+
+public class SpecialTag : TemplatedControl
+{
+    public static readonly StyledProperty<string> SpecialTagNameProperty
+        = AvaloniaProperty.Register<SpecialTag, string>(nameof(SpecialTagName));
+
+    public static readonly StyledProperty<string> TextProperty
+        = AvaloniaProperty.Register<SpecialTag, string>(nameof(Text));
+
+    public string Text
+    {
+        get => GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    public string SpecialTagName
+    {
+        get => GetValue(SpecialTagNameProperty);
+        set => SetValue(SpecialTagNameProperty, value);
+    }
+}
+
