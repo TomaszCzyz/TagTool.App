6144dd2c06993b50b3a41bd3c845e88371bdbf7c
tczyz <tomczyz97@gmail.com>
2023-04-10 23:24:59 +0200

    improve FileSystemSearchView

--- src/TagTool.App/ViewModels/UserControls/FileSystemSearchViewModel.cs
+++ src/TagTool.App/ViewModels/UserControls/FileSystemSearchViewModel.cs
@@ -16,6 +16,7 @@ namespace TagTool.App.ViewModels.UserControls;
 public partial class FileSystemSearchViewModel : Document, IDisposable
 {
     private readonly SearchService.SearchServiceClient _fileSystemSearchService;
+    private readonly TagService.TagServiceClient _tagService;
 
     [ObservableProperty]
     private string _searchRoot = @"C:\Users\tczyz\MyFiles\";
@@ -24,9 +25,15 @@ public partial class FileSystemSearchViewModel : Document, IDisposable
     private string? _currentlySearchDir;
 
     [ObservableProperty]
-    private bool _isSearching;
+    private string? _searchPhrase = "c02791*.svgz";
 
-    public ObservableCollection<string> SearchResults { get; set; } = new();
+    [ObservableProperty]
+    private bool? _searchType;
+
+    [ObservableProperty]
+    private bool _ignoreCase = true;
+
+    public ObservableCollection<TaggableItemViewModel> SearchResults { get; set; } = new();
 
     /// <summary>
     ///     ctor for XAML previewer
@@ -39,12 +46,14 @@ public partial class FileSystemSearchViewModel : Document, IDisposable
         }
 
         _fileSystemSearchService = App.Current.Services.GetRequiredService<ITagToolBackend>().GetFileSystemSearchService();
+        _tagService = App.Current.Services.GetRequiredService<ITagToolBackend>().GetTagService();
     }
 
     [UsedImplicitly]
     public FileSystemSearchViewModel(ITagToolBackend tagToolBackend)
     {
         _fileSystemSearchService = tagToolBackend.GetFileSystemSearchService();
+        _tagService = tagToolBackend.GetTagService();
     }
 
     private AsyncDuplexStreamingCall<SearchRequest, SearchReply>? _streamingCall;
@@ -60,69 +69,76 @@ public partial class FileSystemSearchViewModel : Document, IDisposable
 
         _streamingCall = _fileSystemSearchService.Search();
 
-        var searchRequest = new SearchRequest
-        {
-            Depth = int.MaxValue,
-            Root = _searchRoot,
-            Wildcard = new WildcardExpression { Pattern = "c02791*.svgz" },
-            IgnoreCase = true,
-            ExcludedPaths = { @"C:\Users\tczyz\MyFiles\Documents", @"C:\Users\tczyz\MyFiles\Andrzej" }
-        };
+        var searchRequest = new SearchRequest { Depth = int.MaxValue, Root = SearchRoot, IgnoreCase = IgnoreCase };
 
-        IsSearching = true;
+        switch (SearchType)
+        {
+            case true:
+                searchRequest.Exact = new ExactExpression { Substring = SearchPhrase };
+                break;
+            case null:
+                searchRequest.Wildcard = new WildcardExpression { Pattern = SearchPhrase };
+                break;
+            case false:
+                searchRequest.Regex = new RegexExpression { Pattern = SearchPhrase };
+                break;
+        }
+
         await _streamingCall.RequestStream.WriteAsync(searchRequest);
 
-        Dispatcher.UIThread.Post(async () =>
-        {
-            // cancellationToken???
-            await foreach (var reply in _streamingCall.ResponseStream.ReadAllAsync().WithCancellation(_cts.Token))
-            {
-                switch (reply.ContentCase)
-                {
-                    case SearchReply.ContentOneofCase.FullName:
-                        SearchResults.Add(reply.FullName);
-                        break;
-                    case SearchReply.ContentOneofCase.CurrentlySearchDir:
-                        _currentlySearchDirBuffer = reply.CurrentlySearchDir;
-                        break;
-                }
-            }
+        RunCurrentlySearchDirUpdates();
 
-            IsSearching = false;
-        });
+        await foreach (var reply in _streamingCall.ResponseStream.ReadAllAsync().WithCancellation(_cts.Token))
+        {
+            switch (reply.ContentCase)
+            {
+                case SearchReply.ContentOneofCase.FullName:
+                    var (info, type) = Directory.Exists(reply.FullName)
+                        ? ((FileSystemInfo)new DirectoryInfo(reply.FullName), TaggedItemType.Folder)
+                        : (new FileInfo(reply.FullName), TaggedItemType.File);
+
+                    var taggableItemViewModel = new TaggableItemViewModel(_tagService)
+                    {
+                        TaggedItemType = type,
+                        Size = info is FileInfo fileInfo ? fileInfo.Length : null,
+                        DisplayName = info.Name,
+                        Location = info.FullName,
+                        DateCreated = info.CreationTime,
+                        AreTagsVisible = true,
+                    };
+                    SearchResults.Add(taggableItemViewModel);
+                    break;
+                case SearchReply.ContentOneofCase.CurrentlySearchDir:
+                    _currentlySearchDirBuffer = reply.CurrentlySearchDir;
+                    break;
+            }
+        }
+
+        CurrentlySearchDir = null;
     }
+
+    private void RunCurrentlySearchDirUpdates()
+        => DispatcherTimer.Run(
+            () =>
+            {
+                if (StartSearchCommand.IsRunning)
+                {
+                    CurrentlySearchDir = _currentlySearchDirBuffer;
+                }
+
+                return StartSearchCommand.IsRunning;
+            },
+            TimeSpan.FromMilliseconds(250),
+            DispatcherPriority.Normal);
 
     [RelayCommand]
     private async Task AddExcludedPath(string fullPath)
     {
         if (string.IsNullOrEmpty(fullPath)) return;
 
-        if (IsSearching)
+        if (StartSearchCommand.IsRunning) // not necessary as parameter of this command will always be empty string, when search is not running
         {
             await _streamingCall.RequestStream.WriteAsync(new SearchRequest { ExcludedPaths = { fullPath } });
-        }
-    }
-
-    partial void OnIsSearchingChanged(bool value)
-    {
-        if (value)
-        {
-            DispatcherTimer.Run(
-                () =>
-                {
-                    if (IsSearching)
-                    {
-                        CurrentlySearchDir = _currentlySearchDirBuffer;
-                    }
-
-                    return IsSearching;
-                },
-                TimeSpan.FromMilliseconds(250),
-                DispatcherPriority.Normal);
-        }
-        else
-        {
-            CurrentlySearchDir = null;
         }
     }
 
