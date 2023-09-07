29ee748790ad5d5251a1d64c0c56d43a7347f068
tczyz <tomczyz97@gmail.com>
2023-09-08 01:12:19 +0200

    Introduce newer version of tag associations

--- src/TagTool.App/ViewModels/UserControls/TagsAssociationsViewModel.cs
+++ src/TagTool.App/ViewModels/UserControls/TagsAssociationsViewModel.cs
@@ -1,31 +1,40 @@
 ﻿using System.Collections.ObjectModel;
 using System.Diagnostics;
 using Avalonia.Controls;
+using Avalonia.Threading;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DynamicData;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
+using TagTool.App.Core.Extensions;
 using TagTool.App.Core.Models;
 using TagTool.App.Core.Services;
+using TagTool.App.Core.TagMapper;
 using TagTool.App.Core.ViewModels;
 using TagTool.Backend;
 
 namespace TagTool.App.ViewModels.UserControls;
 
-public record AssociationData(List<ITag> Synonyms, List<ITag> HigherTags);
+public record AssociationData(string GroupName, List<ITag> Synonyms, List<string> Ancestors);
 
 public partial class TagsAssociationsViewModel : ViewModelBase
 {
     private readonly TagService.TagServiceClient _tagService;
 
     [ObservableProperty]
-    private ObservableCollection<AssociationData> _associationData
-        = new()
-        {
-            new AssociationData(new List<ITag> { new TextTag { Name = "Cat" }, new TextTag { Name = "Pussy" } },
-                new List<ITag> { new TextTag { Name = "Animal" }, new TextTag { Name = "Creature" } }),
-            new AssociationData(new List<ITag> { new TextTag { Name = "Dog" } },
-                new List<ITag> { new TextTag { Name = "Animal" }, new TextTag { Name = "Creature" } })
-        };
+    private ObservableCollection<string> _groupNames = new();
+
+    [ObservableProperty]
+    private ObservableCollection<ITag[]> _tagsInGroup = new();
+
+    [ObservableProperty]
+    private ObservableCollection<string[]> _ancestors = new();
+
+    [ObservableProperty]
+    private ObservableCollection<AssociationData> _associationData = new();
 
     /// <summary>
     ///     ctor for XAML previewer
@@ -38,17 +47,51 @@ public partial class TagsAssociationsViewModel : ViewModelBase
         }
 
         _tagService = App.Current.Services.GetRequiredService<ITagToolBackend>().GetTagService();
+        var animalTag = new TextTag { Name = "Animal" };
+        var animalBaseTag = new TextTag { Name = "AnimalBase" };
+        var catTag = new TextTag { Name = "Cat" };
+        var cat2Tag = new TextTag { Name = "Cat2" };
+        var pussyTag = new TextTag { Name = "Pussy" };
+        var dogTag = new TextTag { Name = "Dog" };
+
+        GroupNames.Add("Cat Group");
+        TagsInGroup.Add(new ITag[] { catTag, cat2Tag, pussyTag });
+        Ancestors.Add(new[] { animalTag.DisplayText, animalBaseTag.DisplayText });
+
+        AssociationData.Add(new AssociationData("Animal_TempGroup", new List<ITag> { animalTag }, new List<string> { animalBaseTag.DisplayText }));
+        AssociationData.Add(new AssociationData("AnimalBase_TempGroup", new List<ITag> { animalBaseTag }, new List<string>()));
+        AssociationData.Add(new AssociationData("Cat Group", new List<ITag> { catTag, cat2Tag, pussyTag },
+            new List<string> { animalTag.DisplayText, animalBaseTag.DisplayText }));
+        AssociationData.Add(new AssociationData("Dog Group", new List<ITag> { dogTag },
+            new List<string> { animalTag.DisplayText, animalBaseTag.DisplayText }));
+
+        AssociationData = new ObservableCollection<AssociationData>(AssociationData.OrderBy(data => data.Ancestors.Count).ToArray());
     }
-    
+
     [UsedImplicitly]
     public TagsAssociationsViewModel(ITagToolBackend toolBackend)
     {
         _tagService = toolBackend.GetTagService();
+
+        Initialize();
     }
 
     private void Initialize()
     {
-        
+        var request = new GetAllTagsAssociationsRequest { Tag = Any.Pack(TagMapper.MapToDto(new TextTag { Name = "Cat" })) };
+        var streamingCall = _tagService.GetAllTagsAssociations(request);
+
+        Dispatcher.UIThread.InvokeAsync(async () =>
+        {
+            await foreach (var reply in streamingCall.ResponseStream.ReadAllAsync())
+            {
+                AssociationData.Add(new AssociationData(reply.GroupName, reply.TagsInGroup.MapToDomain().ToList(), reply.ParentGroupNames.ToList()));
+            }
+        });
     }
-    
+
+    // [RelayCommand]
+    // private Task RemoveTagFromSynonyms(ITag tag)
+    // {
+    // }
 }
