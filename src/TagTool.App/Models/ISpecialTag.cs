1a9a5b54532576f533dac8babd784fd9c76f4214
tczyz <tomczyz97@gmail.com>
2023-09-10 00:28:25 +0200

    remove some old, unused stuff

--- src/TagTool.App/Models/ISpecialTag.cs
+++ src/TagTool.App/Models/ISpecialTag.cs
@@ -1,21 +1,3 @@
-﻿using Avalonia.Controls.Documents;
+﻿namespace TagTool.App.Models;
 
-namespace TagTool.App.Models;
-
-public record Tag(string? Name, InlineCollection? Inlines = null);
-
-public interface ISpecialTag
-{
-    string SpecialTagName { get; }
-}
-
-public class NameSpecialTag : ISpecialTag
-{
-    public string FileName { get; init; } = null!;
-
-    public bool CaseSensitive { get; init; }
-
-    public bool MatchSubstrings { get; init; }
-
-    public string SpecialTagName { get; } = nameof(NameSpecialTag);
-}
+public record Tag(string? Name);
