92ecf096bcf2dd4d5e62d2e21f57e478b0626806
tczyz <tomczyz97@gmail.com>
2023-09-15 00:50:22 +0200

    Add template classes for FilePreviewer

---
+++ src/TagTool.App.Core/ViewModels/TaggableItemPreviewerViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TagTool.App.Core.ViewModels;
+
+public partial class TaggableItemPreviewerViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private IImage? _image;
+
+    /// <summary>
+    ///     ctor for XAML previewer
+    /// </summary>
+    public TaggableItemPreviewerViewModel()
+    {
+        if (!Design.IsDesignMode)
+        {
+            Debug.Fail("ctor for XAML Previewer should not be invoke during standard execution");
+        }
+
+        Image = new Bitmap(@"C:\Users\tczyz\Pictures\2022 - Aply\20220326_124320.jpg");
+    }
+}
---
+++ src/TagTool.App.Core/Views/TaggableItemPreviewer.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace TagTool.App.Core.Views;
+
+public partial class TaggableItemPreviewer : UserControl
+{
+    public TaggableItemPreviewer()
+    {
+        InitializeComponent();
+    }
+}
