adb647a2f3154441a920a160e4934adbc4a815b3
tczyz <tomczyz97@gmail.com>
2024-05-05 20:08:01 +0200

    Replace icons with white variants

--- src/TagTool.App.Core/Converters/IconPathToBitmapConverter.cs
+++ src/TagTool.App.Core/Converters/IconPathToBitmapConverter.cs
@@ -1,5 +1,4 @@
-﻿using System.Drawing.Imaging;
-using System.Globalization;
+﻿using System.Globalization;
 using Avalonia.Data.Converters;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
@@ -9,13 +8,13 @@ namespace TagTool.App.Core.Converters;
 
 public class IconPathToBitmapConverter : IValueConverter
 {
-    private const string DefaultFileIconAssetUri = "avares://TagTool.App.Core/Assets/Images/round_description_black_36dp.png";
-    private const string DefaultFolderIconAssetUri = "avares://TagTool.App.Core/Assets/Images/windows_folder_icon.png";
+    private const string DefaultFileIconAssetUri = "avares://TagTool.App.Core/Assets/Images/outline_description_white_24dp.png";
+    private const string DefaultFolderIconAssetUri = "avares://TagTool.App.Core/Assets/Images/outline_folder_white_24dp.png";
 
     private static readonly Stream _defaultFileIcon = AssetLoader.Open(new Uri(DefaultFileIconAssetUri));
     private static readonly Stream _defaultFolderIcon = AssetLoader.Open(new Uri(DefaultFolderIconAssetUri));
 
-    private readonly DefaultFileIconProvider _defaultFileIconProvider = new();
+    private readonly DefaultFileIconProvider _fileIconProvider = new();
 
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
@@ -36,7 +35,7 @@ public class IconPathToBitmapConverter : IValueConverter
             return CreateBitmap(_defaultFolderIcon, length);
         }
 
-        var bitmap = _defaultFileIconProvider.GetFileIcon(path, length);
+        var bitmap = _fileIconProvider.GetFileIcon(path, length);
 
         return bitmap ?? CreateBitmap(_defaultFileIcon, length);
     }
